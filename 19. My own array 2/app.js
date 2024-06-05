bafedfdd29c0f69770ba3977f55ab923dcc7af61
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-06-05 13:08:44 -0700

    Add files via upload

---
+++ 19. My own array 2/app.js
@@ -0,0 +1,3 @@
+"use strict";
+let Languages = ["Urdu", "English", "Spanish", "Italian", "Korean"];
+console.log("Languages I'd like to learn", Languages);
---
+++ 19. My own array 2/app.ts
@@ -0,0 +1,2 @@
+let Languages: string[] = ["Urdu", "English", "Spanish", "Italian", "Korean"]
+console.log("Languages I'd like to learn", Languages);
